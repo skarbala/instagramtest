d645a967d1467bb70f3a0432950f2a6d16cd203c
martin.skarbala <skarbala.martin@gmail.com>
2019-10-07 15:43:03 +0200

    simplified Login steps

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,16 +1,26 @@
 package steps;
 
+import static org.openqa.selenium.By.id;
+
+import static com.codeborne.selenide.Selenide.$;
+
+import static context.DataProvider.getUser;
+import static context.DriverProvider.getAppiumDriver;
+
+import context.DriverProvider;
+import context.User;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import pages.HomePage;
-import pages.LoginPage;
-
-import static context.DataProvider.getUser;
 
 public class LoginSteps {
     @Given("user is logged in")
     public void iAmLoggedIn() {
-        new LoginPage().loginAsUser(getUser("furbo.slav"));
+        User user = getUser("furbo.slav");
+        getAppiumDriver().findElement(id("log_in_button")).click();
+        $(id("login_username")).setValue(user.getUserName());
+        $(id("password")).setValue(user.getPassword());
+        $(id("button_text")).click();
     }
 
     @Then("post appears in feed")
